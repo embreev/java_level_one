7467d8df0df4eb2d4f065728cd22cd3d138a004c
embreev <e.m.breev@gmail.com>
2018-10-20 23:33:55 +0300

    corrected homework on the recommendations of the teacher

--- education/src/main/java/ru/breev/se/Method.java
+++ education/src/main/java/ru/breev/se/Method.java
@@ -4,36 +4,36 @@ public class Method {
     private float res;
 
     //3
-    private float calcExpression(float a, float b, float c, float d){
+    private float calcExpression(float a, float b, float c, float d) {
         if (d != 0) res = a * (b + (c / d));
         return res;
     }
 
     //4
-    private boolean sumExpression(int a, int b){
+    private boolean sumExpression(int a, int b) {
         return ((a + b) > 10 && (a + b) <= 20);
     }
 
     //5
-    public String isPositiveStr(int a){
+    public String isPositiveStr(int a) {
         if (a <= 0) return "Posivive";
         else return "Negative";
     }
 
     //6
-    public boolean isPositiveBool(int a){
+    public boolean isPositiveBool(int a) {
         return (a < 0);
     }
 
     //7
-    private void hello(String name){
+    private void sayHello(String name) {
         System.out.printf("Hello, %s!\n", name);
     }
 
     //8
-    private String isLeapYear(int date){
-        if((date % 4 == 0) && (date % 100 != 0) || (date % 400 == 0)) return "Year leap";
-        else return "Year not leap";
+    private String isLeapYear(int date) {
+        final boolean check = (date % 4 == 0) && (date % 100 != 0) || (date % 400 == 0);
+        return check ? "Year Leap" : "Year Not Leap";
     }
 
     public static void main(String[] args) {
@@ -48,7 +48,7 @@ public class Method {
         //6
         System.out.println(res.isPositiveBool(-2));
         //7
-        res.hello("Евгений");
+        res.sayHello("Евгений");
         //8
         System.out.println(res.isLeapYear(2018));
 //        for (int i=1600; i<=2018; i+=1){

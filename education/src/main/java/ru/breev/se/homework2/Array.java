97aab98a06e012393ad8ab39bc6391743d22209e
embreev <e.m.breev@gmail.com>
2018-10-21 16:11:05 +0300

    completed task number 2

--- education/src/main/java/ru/breev/se/homework2/Array.java
+++ education/src/main/java/ru/breev/se/homework2/Array.java
@@ -1,13 +1,24 @@
 package ru.breev.se.homework2;
 
 public class Array {
-    static final byte[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+    static final byte[] arrBit = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+    static final int[] arrInt = new int[8];
 
     public static void main(String[] args) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (byte) (arr[i] == 1 ? 0 : 1);
+        //1
+        for (int i = 0; i < arrBit.length; i++) {
+            arrBit[i] = (byte) (arrBit[i] == 1 ? 0 : 1);
         }
-        for (byte element : arr) {
+        for (byte element : arrBit) {
+            System.out.printf("%s ", element);
+        }
+        //2
+        System.out.println();
+        for (int i = 0; i < arrInt.length; i++) {
+            if (i == 0) arrInt[i] = 0;
+            else arrInt[i] = arrInt[i - 1] + 3;
+        }
+        for (int element : arrInt) {
             System.out.printf("%s ", element);
         }
     }

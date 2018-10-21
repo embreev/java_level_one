a0fa48a8a5e7750701c30acfac8978cf8d58101f
embreev <e.m.breev@gmail.com>
2018-10-21 17:11:13 +0300

    completed task number 4

--- education/src/main/java/ru/breev/se/homework2/Array.java
+++ education/src/main/java/ru/breev/se/homework2/Array.java
@@ -4,6 +4,7 @@ public class Array {
     private static final byte[] arrBit = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
     private static final int[] arrInt = new int[8];
     private static final int[] arrNew = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    private static final int[][] arrSquare = new int[10][10];
 
     public static void main(String[] args) {
         //1
@@ -14,7 +15,7 @@ public class Array {
             System.out.printf("%s ", element);
         }
         //2
-        System.out.println();
+        System.out.printf("\n\n");
         for (int i = 0; i < arrInt.length; i++) {
             if (i == 0) arrInt[i] = 0;
             else arrInt[i] = arrInt[i - 1] + 3;
@@ -23,12 +24,26 @@ public class Array {
             System.out.printf("%s ", element);
         }
         //3
-        System.out.println();
+        System.out.printf("\n\n");
         for (int i = 0; i < arrNew.length; i++) {
             if (arrNew[i] < 6) arrNew[i] *= 2;
         }
         for (int element : arrNew) {
             System.out.printf("%s ", element);
+        }
+        //4
+        System.out.printf("\n\n");
+        for (int i = 0; i < arrSquare.length; i++) {
+            for (int j = 0; j < arrSquare.length; j++) {
+                if (i == j) arrSquare[i][j] = 1;
+                else arrSquare[i][j] = (int) (Math.random() * 10);
+            }
+        }
+        for (int i = 0; i < arrSquare.length; i++) {
+            for (int j = 0; j < arrSquare.length; j++) {
+                System.out.printf("%s ", arrSquare[i][j]);
+            }
+            System.out.println();
         }
     }
 }

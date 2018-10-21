7bb8f9e48dcb0f8b547e6d296c93dfbadcf647d8
embreev <e.m.breev@gmail.com>
2018-10-21 15:51:50 +0300

    test

--- education/src/main/java/ru/breev/se/homework2/Array.java
+++ education/src/main/java/ru/breev/se/homework2/Array.java
@@ -1,4 +1,14 @@
 package ru.breev.se.homework2;
 
 public class Array {
+    static final byte[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (byte) (arr[i] == 1 ? 0 : 1);
+        }
+        for (byte element : arr) {
+            System.out.printf("%s ", element);
+        }
+    }
 }

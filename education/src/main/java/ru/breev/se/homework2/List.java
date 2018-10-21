04d3fadc4dccc38d158cc59ea188c051105c5694
embreev <e.m.breev@gmail.com>
2018-10-21 21:58:50 +0300

    completed task number 5

--- education/src/main/java/ru/breev/se/homework2/List.java
+++ education/src/main/java/ru/breev/se/homework2/List.java
@@ -1,4 +1,19 @@
 package ru.breev.se.homework2;
 
 public class List {
+    public static void main(String[] args) {
+        int[] array = new int[50];
+        int min = 0;
+        int max = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+        for (int element : array) {
+            if (element > max) max = element;
+            if (element < min) min = element;
+            System.out.printf("%s ", element);
+        }
+        System.out.printf("\nMin = %s, Max = %s", min, max);
+    }
 }

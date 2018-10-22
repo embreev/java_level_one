1db5d57943ffa4b1b63f7067a1d325c2cb2c5271
embreev <e.m.breev@gmail.com>
2018-10-22 21:45:11 +0300

    completed task number 6

--- education/src/main/java/ru/breev/se/homework2/ListExample.java
+++ education/src/main/java/ru/breev/se/homework2/ListExample.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ListExample {
     public static void main(String[] args) {
+        //5
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter array dimension:");
         int dimension = scanner.nextInt();
@@ -11,6 +12,9 @@ public class ListExample {
         int[] newList = list.fillArray(dimension);
         for (int element : newList) System.out.printf("%s ", element);
         System.out.printf("\nMin = %s, Max = %s", list.getMin(newList), list.getMax(newList));
+        //6
+        int[] testArray = {1, 2, 3};
+        System.out.printf("\n%s", list.checkBalance(testArray));
     }
 
     public int[] fillArray(int dimension) {
@@ -35,5 +39,22 @@ public class ListExample {
             if (element > max) max = element;
         }
         return max;
+    }
+
+    public boolean checkBalance(int[] array) {
+        int a = 0;
+        int b;
+        boolean result = false;
+        for (int i = 0; i < array.length - 1; i++) {
+            a += array[i];
+            b = 0;
+            int step = i + 1;
+            for (int j = step; j < array.length; j++) {
+                b += array[j];
+            }
+            result = (a == b) ? true : false;
+            if (result) break;
+        }
+        return result;
     }
 }

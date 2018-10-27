29c3c240608933f1eae0fc3aaede0a17ee96d2a1
embreev <e.m.breev@gmail.com>
2018-10-27 14:30:58 +0300

    add two methods: choose random word from array compare user and computer words

--- education/src/main/java/ru/breev/se/homework3/GameGuessWord.java
+++ education/src/main/java/ru/breev/se/homework3/GameGuessWord.java
@@ -1,4 +1,33 @@
 package ru.breev.se.homework3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GameGuessWord {
+    public static void main(String[] args) {
+        boolean answer = true;
+        System.out.printf("Enter word:\n");
+        Scanner sc = new Scanner(System.in);
+        GameGuessWord w = new GameGuessWord();
+        String userWord = sc.nextLine().toLowerCase();
+        String computerWord = w.chooseRandomWord();
+        if (w.compareWord(userWord, computerWord)) System.out.printf("User WIN!");
+        else System.out.printf("User Word = %s, Computer Word = %s\n", userWord, computerWord);
+    }
+
+    private static String chooseRandomWord() {
+        final String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+                "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak",
+                "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
+                "pepper", "pineapple", "pumpkin", "potato"};
+        Random r = new Random();
+        String randomWord = words[r.nextInt(words.length)];
+        return randomWord;
+    }
+
+    private static boolean compareWord(String userWord, String computerWord) {
+        boolean result = false;
+        if (userWord.equals(computerWord)) result = true;
+        return result;
+    }
 }

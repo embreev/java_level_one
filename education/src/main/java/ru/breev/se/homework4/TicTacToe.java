19778ef47f60a7edb7741c4740bcdb816f6e25de
embreev <e.m.breev@gmail.com>
2018-10-29 10:17:41 +0300

    update method drawField, add method refillArray

--- education/src/main/java/ru/breev/se/homework4/TicTacToe.java
+++ education/src/main/java/ru/breev/se/homework4/TicTacToe.java
@@ -4,28 +4,59 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         final char userBlock = 'X';
         final char computerBlock = 'O';
         final char emptyBlock = '.';
         final TicTacToe game = new TicTacToe();
         System.out.printf("Run the Game\n");
-        final Scanner scanner = new Scanner(System.in);
         System.out.printf("Enter file size:\n");
         final int fieldSize = scanner.nextInt();
-        System.out.printf("Enter the number of winning blocks:\n");
-        final int winBlock = scanner.nextInt();
-        final char[][] array = game.fillArray(fieldSize);
+//        System.out.printf("Enter the number of winning blocks:\n");
+//        final int winBlock = scanner.nextInt();
+        char[][] array = game.fillArray(fieldSize);
         game.drawField(array);
+        int userX = game.goPlayer('x', fieldSize);
+        int userY = game.goPlayer('y', fieldSize);
+        array = game.refillArray(array, userX, userY, "user");
+        game.drawField(array);
+//        game.checkWin(array);
+//        game.goPC();
+    }
+
+//    private boolean checkWin(char[][] array) {
+//        for (int x; x < array.length; x++) {
+//            for (int y; y < array.length; y++) {
+//                array
+//            }
+//
+//        }
+//        return win;
+//    }
+
+    private int goPlayer(char coordinate, int fieldSize) {
+        System.out.printf("Enter coordinate - %s:\n", coordinate);
+        int userCoordinate = scanner.nextInt();
+        if (userCoordinate > fieldSize) {
+            System.out.printf("Incorrect coordinate...\n");
+            goPlayer(coordinate, fieldSize);
+        }
+        return userCoordinate - 1;
     }
 
     private void drawField(char[][] array) {
-        for (int y = 0; y < array.length; y++) {
-            for (int x = 0; x < array.length; x++) {
+        for (int x = 0; x <= array.length; x++) System.out.printf("%s ", x);
+        System.out.printf("\n");
+        for (int x = 0; x < array.length; x++) {
+            System.out.printf("%s ", x + 1);
+            for (int y = 0; y < array.length; y++) {
                 System.out.printf("%s ", array[x][y]);
             }
             System.out.print("\n");
         }
+        System.out.printf("\n");
     }
 
     private char[][] fillArray(int fieldSize) {
@@ -35,6 +66,12 @@ public class TicTacToe {
                 array[x][y] = '.';
             }
         }
+        return array;
+    }
+
+    private char[][] refillArray(char[][] array, int coordinateX, int coordinateY, String whoseTurn) {
+        if (whoseTurn.equals("user")) array[coordinateX][coordinateY] = 'X';
+        if (whoseTurn.equals("pc")) array[coordinateX][coordinateY] = 'O';
         return array;
     }
 }

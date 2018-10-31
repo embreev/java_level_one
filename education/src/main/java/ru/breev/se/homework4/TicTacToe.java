4e68ef8fc293069e5d824adc21c754f6ece2a119
embreev <e.m.breev@gmail.com>
2018-10-31 03:10:17 +0300

    add Computer AI

--- education/src/main/java/ru/breev/se/homework4/TicTacToe.java
+++ education/src/main/java/ru/breev/se/homework4/TicTacToe.java
@@ -24,7 +24,7 @@ public class TicTacToe {
             System.out.printf("Enter the number of winning cells:\n");
             winLine = scanner.nextInt();
         } while (fieldSize < winLine);
-        char[][] array = game.fillArray(fieldSize, EMPTY_CELL);
+        final char[][] array = game.fillArray(fieldSize, EMPTY_CELL);
         game.drawField(array);
         while (true) {
             game.checkEmpty(array);
@@ -35,7 +35,8 @@ public class TicTacToe {
                 break;
             }
             game.checkEmpty(array);
-            game.goComputer(array, fieldSize);
+            if (!game.goComputer(array, fieldSize, winLine) && !game.isFinalStep(array, fieldSize, winLine))
+                goRandomComputer(array, fieldSize, winLine);
             game.drawField(array);
             if (game.checkWin(array, COMPUTER_CELL, winLine)) {
                 System.out.printf("Computer WIN!\n");
@@ -45,6 +46,7 @@ public class TicTacToe {
     }
 
     private boolean checkWin(char[][] array, char block, int winLine) {
+        //Diagonal
         int diagonalCounter = 0;
         for (int x = 0; x < array.length; x++) {
             for (int y = 0; y < array.length; y++) {
@@ -52,6 +54,7 @@ public class TicTacToe {
             }
         }
         if (diagonalCounter != winLine) diagonalCounter = 0;
+        //Reverse diagonal
         int diagonalReversCounter = 0;
         for (int x = 0; x < array.length; x++) {
             for (int y = (array.length - 1); y >= 0; y--) {
@@ -59,12 +62,13 @@ public class TicTacToe {
             }
         }
         if (diagonalReversCounter != winLine) diagonalReversCounter = 0;
+        //Row and Column
         int rowCounter = 0;
         int columnCounter = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i][j] == block) rowCounter += 1;
-                if (array[j][i] == block) columnCounter += 1;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array.length; y++) {
+                if (array[x][y] == block) rowCounter += 1;
+                if (array[y][x] == block) columnCounter += 1;
             }
             if (rowCounter != winLine) rowCounter = 0;
             else break;
@@ -101,12 +105,162 @@ public class TicTacToe {
         array[x][y] = USER_CELL;
     }
 
-    private void goComputer(char[][] array, int fieldSize) {
+    private boolean goComputer(char[][] array, int fieldSize, int winLine) {
+        //Diagonal
+        int diagonalCounter = 0;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array.length; y++) {
+                if ((x == y) && (array[x][y] == USER_CELL)) diagonalCounter += 1;
+            }
+        }
+        if (winLine - diagonalCounter == 1) {
+            for (int x = 0; x < array.length; x++) {
+                for (int y = 0; y < array.length; y++) {
+                    if ((x == y) && (array[x][y] == EMPTY_CELL)) {
+                        array[x][y] = COMPUTER_CELL;
+                        System.out.printf("Computer x=%s, y=%s\n", x + 1, y + 1);
+                        return true;
+                    }
+                }
+            }
+        }
+        //Reverse diagonal
+        int diagonalReversCounter = 0;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = (array.length - 1); y >= 0; y--) {
+                if (x == (array.length - y - 1) && (array[x][y] == USER_CELL)) diagonalReversCounter += 1;
+            }
+        }
+        if ((winLine - diagonalReversCounter) == 1) {
+            for (int x = 0; x < array.length; x++) {
+                for (int y = (array.length - 1); y >= 0; y--) {
+                    if (x == (array.length - y - 1) && (array[x][y] == EMPTY_CELL)) {
+                        array[x][y] = COMPUTER_CELL;
+                        System.out.printf("Computer x=%s, y=%s\n", x + 1, y + 1);
+                        return true;
+                    }
+                }
+            }
+        }
+        //Row
+        int rowCounter = 0;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array.length; y++) {
+                if (array[x][y] == USER_CELL) rowCounter += 1;
+            }
+            if ((winLine - rowCounter) == 1) {
+                for (int y = 0; y < array.length; y++) {
+                    if (array[x][y] == EMPTY_CELL) {
+                        array[x][y] = COMPUTER_CELL;
+                        System.out.printf("Computer x=%s, y=%s\n", x + 1, y + 1);
+                        return true;
+                    }
+                }
+            }
+            rowCounter = 0;
+        }
+        //Column
+        int columnCounter = 0;
+        for (int y = 0; y < array.length; y++) {
+            for (int x = 0; x < array.length; x++) {
+                if (array[x][y] == USER_CELL) columnCounter += 1;
+            }
+            if ((winLine - columnCounter) == 1) {
+                for (int x = 0; x < array.length; x++) {
+                    if (array[x][y] == EMPTY_CELL) {
+                        array[x][y] = COMPUTER_CELL;
+                        System.out.printf("Computer x=%s, y=%s\n", x + 1, y + 1);
+                        return true;
+                    }
+                }
+            }
+            columnCounter = 0;
+        }
+        return false;
+    }
+
+    private boolean isFinalStep(char[][] array, int fieldSize, int winLine) {
+        //Diagonal
+        int diagonalCounter = 0;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array.length; y++) {
+                if ((x == y) && (array[x][y] == COMPUTER_CELL)) diagonalCounter += 1;
+            }
+        }
+        if (winLine - diagonalCounter == 1) {
+            for (int x = 0; x < array.length; x++) {
+                for (int y = 0; y < array.length; y++) {
+                    if ((x == y) && (array[x][y] == EMPTY_CELL)) {
+                        array[x][y] = COMPUTER_CELL;
+                        System.out.printf("Computer x=%s, y=%s\n", x + 1, y + 1);
+                        return true;
+                    }
+                }
+            }
+        }
+        //Reverse diagonal
+        int diagonalReversCounter = 0;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = (array.length - 1); y >= 0; y--) {
+                if (x == (array.length - y - 1) && (array[x][y] == COMPUTER_CELL)) diagonalReversCounter += 1;
+            }
+        }
+        if ((winLine - diagonalReversCounter) == 1) {
+            for (int x = 0; x < array.length; x++) {
+                for (int y = (array.length - 1); y >= 0; y--) {
+                    if (x == (array.length - y - 1) && (array[x][y] == EMPTY_CELL)) {
+                        array[x][y] = COMPUTER_CELL;
+                        System.out.printf("Computer x=%s, y=%s\n", x + 1, y + 1);
+                        return true;
+                    }
+                }
+            }
+        }
+        //Row
+        int rowCounter = 0;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array.length; y++) {
+                if (array[x][y] == COMPUTER_CELL) rowCounter += 1;
+            }
+            if ((winLine - rowCounter) == 1) {
+                for (int y = 0; y < array.length; y++) {
+                    if (array[x][y] == EMPTY_CELL) {
+                        array[x][y] = COMPUTER_CELL;
+                        System.out.printf("Computer x=%s, y=%s\n", x + 1, y + 1);
+                        return true;
+                    }
+                }
+            }
+            break;
+        }
+        //Column
+        int columnCounter = 0;
+        for (int y = 0; y < array.length; y++) {
+            for (int x = 0; x < array.length; x++) {
+                if (array[x][y] == COMPUTER_CELL) columnCounter += 1;
+            }
+            if ((winLine - columnCounter) == 1) {
+                for (int x = 0; x < array.length; x++) {
+                    if (array[x][y] == EMPTY_CELL) {
+                        array[x][y] = COMPUTER_CELL;
+                        System.out.printf("Computer x=%s, y=%s\n", x + 1, y + 1);
+                        return true;
+                    }
+                }
+            }
+            break;
+        }
+        return false;
+    }
+
+    //    Random move computer
+    private void goRandomComputer(char[][] array, int fieldSize, int winLine) {
         int x;
         int y;
+        final Random random = new Random();
         do {
-            x = new Random().nextInt(fieldSize);
-            y = new Random().nextInt(fieldSize);
+            x = random.nextInt(fieldSize);
+            y = random.nextInt(fieldSize);
         } while (game.checkCell(array, x, y, fieldSize));
         array[x][y] = COMPUTER_CELL;
         System.out.printf("Computer x=%s, y=%s\n", x + 1, y + 1);
@@ -115,7 +269,7 @@ public class TicTacToe {
     private boolean checkCell(char[][] array, int x, int y, int fieldSize) {
         boolean result = (x < 0 || x >= fieldSize || y < 0 || y >= fieldSize ||
                 array[x][y] != EMPTY_CELL) ? true : false;
-        if (result) System.out.printf("Incorrect coordinate...\n");
+//        if (result) System.out.printf("Incorrect coordinate...\n");
         return result;
     }
 
@@ -142,5 +296,3 @@ public class TicTacToe {
         return array;
     }
 }
-
-

9fc3cab949383c8ebfef38e7956906651dca94ec
embreev <e.m.breev@gmail.com>
2018-11-23 20:56:58 +0300

    Add two class Cat and Foot

---
+++ education/src/main/java/ru/breev/se/homework7/Plate.java
@@ -0,0 +1,17 @@
+package ru.breev.se.homework7;
+
+public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+}
